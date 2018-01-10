b9aed522faf7c0f2da48d6beb3734caa580a4eb0
tuna <931309012@qq.com>
2018-01-10 13:54:31 +0800

    对controller的listall方法，增加user：view权限校验

--- src/main/java/com/aixin/controller/Hello.java
+++ src/main/java/com/aixin/controller/Hello.java
@@ -1,19 +1,16 @@
 package com.aixin.controller;
 
 
-import com.aixin.mapper.SysroleMapper;
-import com.aixin.model.Sysrole;
 import com.aixin.model.Userinfo;
 //import com.aixin.service.RoleService;
+import com.aixin.service.PermissionService;
 import com.aixin.service.RoleService;
 import com.aixin.service.UserInfoService;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.annotation.Resource;
-import javax.websocket.server.PathParam;
 import java.util.List;
 
 /**
@@ -25,6 +22,8 @@ public class Hello {
     private UserInfoService userInfoService;
     @Autowired
     RoleService roleService;
+    @Autowired
+    PermissionService permissionService;
 
     @RequestMapping("/hello")
     @ResponseBody
@@ -33,19 +32,27 @@ public class Hello {
         return userInfo;
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index(Model model) {
-
-        model.addAttribute("name", "dailin");
-
-        return "login";
-    }
 
     @RequestMapping("/role/{userName}")
     @ResponseBody
-    public List<Sysrole> role(@PathVariable(value = "userName") String username){
-        List<Sysrole> sysroles = roleService.getRoles(username);
+    public List<String> role(@PathVariable(value = "userName") String username){
+        List<String> sysroles = roleService.getRoles(username);
         return sysroles;
+    }
+
+    @RequestMapping("/permission")
+    @ResponseBody
+    public List<String> permission(){
+        List<String> sysroles = permissionService.getPermissones("1");
+        return sysroles;
+    }
+
+    @RequestMapping("/listall")
+    @ResponseBody
+    @RequiresPermissions("userInfo:view")
+    public List<Userinfo> list(){
+        List<Userinfo> members = userInfoService.getMembers();
+        return members;
     }
 
 }
